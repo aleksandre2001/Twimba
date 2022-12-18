650f59bbdcc9f4fecc06d831bfbb2ac90c3538fc
Aleksandre <58823462+aleksandre2001@users.noreply.github.com>
2022-12-18 15:15:26 +0400

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
+import images from '../public/images'
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `troll.jpg`,
+        profilePic: `images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
